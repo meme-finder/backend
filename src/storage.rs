6cb5c152079420d86005a838b33412b6b06db154
Alexander Averyanov <alex@averyan.ru>
2022-05-02 21:41:54 +0300

    File with full id

--- src/storage.rs
+++ src/storage.rs
@@ -30,49 +30,48 @@ pub async fn save_images(
 ) -> Result<(), Box<dyn Error>> {
     let id = uuid.to_string();
     let path = format!("{}/{}", &id[..2], &id[2..4]);
-    let name = &id[4..];
     let base = env::var("IMAGES_DIR").unwrap_or_else(|_| String::from("./storage/images"));
 
     async_create_dir_all(format!("{base}/full/{path}")).await?;
-    write_to_file(format!("{base}/full/{path}/{name}.webp"), images.full.webp).await?;
-    write_to_file(format!("{base}/full/{path}/{name}.jpeg"), images.full.jpeg).await?;
-    write_to_file(format!("{base}/full/{path}/{name}.png"), images.full.png).await?;
+    write_to_file(format!("{base}/full/{path}/{id}.webp"), images.full.webp).await?;
+    write_to_file(format!("{base}/full/{path}/{id}.jpeg"), images.full.jpeg).await?;
+    write_to_file(format!("{base}/full/{path}/{id}.png"), images.full.png).await?;
 
     async_create_dir_all(format!("{base}/normal/{path}")).await?;
     write_to_file(
-        format!("{base}/normal/{path}/{name}.webp"),
+        format!("{base}/normal/{path}/{id}.webp"),
         images.normal.webp,
     )
     .await?;
     write_to_file(
-        format!("{base}/normal/{path}/{name}.jpeg"),
+        format!("{base}/normal/{path}/{id}.jpeg"),
         images.normal.jpeg,
     )
     .await?;
     write_to_file(
-        format!("{base}/normal/{path}/{name}.png"),
+        format!("{base}/normal/{path}/{id}.png"),
         images.normal.png,
     )
     .await?;
 
     async_create_dir_all(format!("{base}/preview/{path}")).await?;
     write_to_file(
-        format!("{base}/preview/{path}/{name}.webp"),
+        format!("{base}/preview/{path}/{id}.webp"),
         images.preview.webp,
     )
     .await?;
     write_to_file(
-        format!("{base}/preview/{path}/{name}.jpeg"),
+        format!("{base}/preview/{path}/{id}.jpeg"),
         images.preview.jpeg,
     )
     .await?;
     write_to_file(
-        format!("{base}/preview/{path}/{name}.png"),
+        format!("{base}/preview/{path}/{id}.png"),
         images.preview.png,
     )
     .await?;
 
     async_create_dir_all(format!("{base}/original/{path}")).await?;
-    write_to_file(format!("{base}/original/{path}/{name}"), images.original).await?;
+    write_to_file(format!("{base}/original/{path}/{id}"), images.original).await?;
     Ok(())
 }
